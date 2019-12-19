4071b356b70b89b77e882f628d706e7803dff597
Angel Pons <th3fanbus@gmail.com>
2019-12-19 19:37:21 +0100

    mb/lenovo/t540p/hda_verb: use denary numerals for codec IDs
    Denary, also known as "decimal" or "base 10," is the standard
    number system used around the world. Therefore, make use of it.
    
    See also commit ec21170b with Change-Id:
    I7f2937bb7715e0769db3be8cb30d305f9d78b6f8 ("mb/**/hda_verb.{c,h}: use
    denary numerals for codec IDs").
    
    Change-Id: I66491d161bd56823b404a7e21b7f3f2c915cce9e
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/mainboard/lenovo/t540p/hda_verb.c
+++ src/mainboard/lenovo/t540p/hda_verb.c
@@ -21,10 +21,10 @@ const u32 cim_verb_data[] = {
 	0x80862807,	/* Codec Vendor / Device ID: Intel */
 	0x80860101,	/* Subsystem ID */
 	4,		/* Number of 4 dword sets */
-	AZALIA_SUBVENDOR(0x0, 0x80860101),
-	AZALIA_PIN_CFG(0x0, 0x05, 0x18560010),
-	AZALIA_PIN_CFG(0x0, 0x06, 0x18560020),
-	AZALIA_PIN_CFG(0x0, 0x07, 0x18560030),
+	AZALIA_SUBVENDOR(0, 0x80860101),
+	AZALIA_PIN_CFG(0, 0x05, 0x18560010),
+	AZALIA_PIN_CFG(0, 0x06, 0x18560020),
+	AZALIA_PIN_CFG(0, 0x07, 0x18560030),
 };
 
 const u32 pc_beep_verbs[0] = {};
